a8b487975b325a96573da6781e085c4ca8878227
02DPNDombrovskis <78017805+MiNTwastaken@users.noreply.github.com>
2023-11-26 20:52:38 +0200

    Cookie setup (work in progress)

--- daily.php
+++ daily.php
@@ -1,98 +1,114 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Valorant Guesser</title>
-		<link rel="stylesheet" href="styles.css">
-	</head>
-	<body>
-		<?php
-		session_start();
+<?php
+session_start();
 
-		// Set up database connection (replace host, username, password, and database with your own values)
-		$connection = mysqli_connect("localhost:3306", "root", "", "valorantguesser");
+// Generate a unique identifier for the user and store it in a cookie
+if (!isset($_COOKIE["user_identifier"])) {
+    $userIdentifier = uniqid("user_", true);
+    setcookie("user_identifier", $userIdentifier, time() + (86400 * 30), "/"); // Cookie lasts for 30 days
+}
 
-		// Check connection
-		if (!$connection) {
-			die("Connection failed: " . mysqli_connect_error());
-		}
+// Set up database connection
+$connection = mysqli_connect("localhost:3306", "root", "", "valorantguesser");
 
-		if (!isset($_SESSION["tries"]) || !isset($_SESSION["table_index"]) || !isset($_SESSION["answer"])) {
-			$_SESSION["tries"] = 5;
-			$_SESSION["table_index"] = 0;
-			$_SESSION["answer"] = "";
-		}
-		
-		// If form is submitted, process the guess
-		if (isset($_POST["submit"])) {
-			$guess = $_POST["guess"];
-			if ($guess === $_SESSION["answer"]) {
-				$_SESSION["tries"] = 5;
-				$_SESSION["answer"] = "";
-				$_SESSION["table_index"]++;
-				// Reset table index to 0 if it exceeds the number of tables
-				if ($_SESSION["table_index"] >= 6) {
-					header("Location: input.php");
-	                exit();
-				}
-			} else {
-				$_SESSION["tries"]--;
-			}
-		}
-		else {
-			// Only set initial game state if the form has not been submitted
-			if (!isset($_SESSION["tries"])) {
-				$_SESSION["tries"] = 5;
-			}
-			if (!isset($_SESSION["table_index"])) {
-				$_SESSION["table_index"] = 0;
-			}
-			if (!isset($_SESSION["answer"])) {
-				$_SESSION["answer"] = "";
-			}
-		}
-		
-		// If there are no more tables or no more tries left, end the game
-		if ($_SESSION["table_index"] >= 6 || $_SESSION["tries"] <= 0) {
-			echo "Game over";
-			echo "<form method='post'><input type='submit' name='try_again' value='Try again'></form>";
-			session_destroy();
-		} else {
-			// Select a random image path or quote and answer from the current table
-			$tables = array("ability", "agent", "graffiti", "playercard", "weapon", "quote");
-			$table = $tables[$_SESSION["table_index"]];
-			if ($table === "quote") {
-				$query = "SELECT quote, answer FROM quote WHERE id = 1";
-			} else {
-				$query = "SELECT answer, img FROM $table WHERE id = 1";
-			}
-			$result = mysqli_query($connection, $query);
-			if (mysqli_num_rows($result) > 0) {
-				$row = mysqli_fetch_assoc($result);
-				$_SESSION["answer"] = $row["answer"];
-				if ($table === "quote") {
-					$quote = $row["quote"];
-					$image_path = "";
-				} else {
-					$image_path = $row["img"];
-					$quote = "";
-				}
-			}
+// Check connection
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
-			// Output the image or quote and the guess form
-			if ($table === "quote") {
-				echo "<blockquote class='centered'>$quote</blockquote><br>";
-			} else {
-				echo "<img class='img-size' src='$image_path'><br>";
-			}
-			echo "<form method='post'>";
-			echo "<label for='guess'>Guess:</label>";
-			echo "<input type='text' name='guess' id='guess'>";
-			echo "<input type='submit' name='submit' value='Submit'>";
-			echo "</form>";
-			echo "Tries left: {$_SESSION['tries']}";
-		}
-		// Close database connection
-		mysqli_close($connection);
-		?>
-	</body>
-</html>
+if (!isset($_SESSION["tries"]) || !isset($_SESSION["table_index"]) || !isset($_SESSION["name"]) || !isset($_SESSION["quote"]) || !isset($_SESSION["image_path"])) {
+    $_SESSION["tries"] = 5;
+    $_SESSION["table_index"] = 0;
+    $_SESSION["name"] = "";
+    $_SESSION["quote"] = "";
+    $_SESSION["image_path"] = "";
+}
+
+// If form is submitted, process the guess
+if (isset($_POST["submit"])) {
+    $guess = $_POST["guess"];
+    if ($guess === $_SESSION["name"]) {
+        $_SESSION["tries"] = 5;
+        $_SESSION["name"] = "";
+        $_SESSION["quote"] = "";
+        $_SESSION["image_path"] = "";
+        $_SESSION["table_index"]++;
+        // Reset table index to 0 if it exceeds the number of tables
+        if ($_SESSION["table_index"] >= 6) {
+            // Display "You've won"
+            echo "You've won!";
+            echo "<form method='post'><input type='submit' name='retry' value='Retry'>";
+            echo "<a href='index.php'><button type='button'>Main Screen</button></a></form>";
+            session_destroy();
+            exit; // Stop execution to prevent further output
+        }
+    } else {
+        $_SESSION["tries"]--;
+    }
+} else {
+    // Only set initial game state if the form has not been submitted
+    if (!isset($_SESSION["tries"])) {
+        $_SESSION["tries"] = 5;
+    }
+    if (!isset($_SESSION["table_index"])) {
+        $_SESSION["table_index"] = 0;
+    }
+    if (!isset($_SESSION["name"])) {
+        $_SESSION["name"] = "";
+    }
+    if (!isset($_SESSION["quote"])) {
+        $_SESSION["quote"] = "";
+    }
+    if (!isset($_SESSION["image_path"])) {
+        $_SESSION["image_path"] = "";
+    }
+}
+
+// If there are no more tables or no more tries left, end the game
+if ($_SESSION["table_index"] >= 6 || $_SESSION["tries"] <= 0) {
+    echo "Game over";
+    echo "<form method='post'><input type='submit' name='try_again' value='Try again'>";
+    echo "<a href='index.php'><button type='button'>Main Screen</button></a></form>";
+    session_destroy();
+} else {
+    // Select a random image path or quote and answer from the current table
+    $tables = array("ability", "agent", "graffiti", "playercard", "weapon", "quote");
+    $table = $tables[$_SESSION["table_index"]];
+
+    // Retrieve preselected question from the dailychallenge table based on user preferences
+    $userIdentifier = $_COOKIE["user_identifier"];
+
+    $query = "SELECT $table.`name`, $table.img, quote.quote
+              FROM dailychallenge
+              JOIN $table ON dailychallenge.${table}_id = $table.${table}_id
+              LEFT JOIN quote ON dailychallenge.quote_id = quote.quote_id
+              WHERE dailychallenge.user_identifier = '$userIdentifier'
+              ORDER BY RAND() LIMIT 1";
+
+    $result = mysqli_query($connection, $query);
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        $_SESSION["name"] = $row["name"];
+        if ($table === "quote") {
+            $_SESSION["quote"] = $row["quote"];
+        } else {
+            $_SESSION["image_path"] = $row["img"];
+        }
+    }
+
+    // Output the image or quote and the guess form
+    if ($table === "quote") {
+        echo "<blockquote class='centered'>{$_SESSION['quote']}</blockquote><br>";
+    } else {
+        echo "<img class='img-size' src='{$_SESSION['image_path']}'><br>";
+    }
+    echo "<form method='post'>";
+    echo "<label for='guess'>Guess:</label>";
+    echo "<input type='text' name='guess' id='guess'>";
+    echo "<input type='submit' name='submit' value='Submit'>";
+    echo "</form>";
+    echo "Tries left: {$_SESSION['tries']}";
+}
+
+// Close database connection
+mysqli_close($connection);
+?>
